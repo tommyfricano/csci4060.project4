a929791d32740acb8f1e244408f794896c2f6a0b
Tommy <tjfricano@gmail.com>
2022-10-26 01:22:42 -0400

    Adds basic navigation through the tool bar and menu drawers.

---
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/StartNewQuizFragment.java
@@ -0,0 +1,28 @@
+package edu.uga.cs.statecapitalsquiz;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class StartNewQuizFragment extends Fragment {
+
+    public StartNewQuizFragment() {
+        // Required empty public constructor
+    }
+
+    public static StartNewQuizFragment newInstance() {
+        StartNewQuizFragment fragment = new StartNewQuizFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz_layout, container, false);
+    }
+
+}
