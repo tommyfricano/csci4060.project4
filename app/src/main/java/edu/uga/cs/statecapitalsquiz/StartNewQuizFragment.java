5ea7ae7da4bc07c259c3453ce64879aa610c3a6b
Tommy <tjfricano@gmail.com>
2022-10-26 10:33:35 -0400

    Adds csv file with capitals in assets folder

--- app/src/main/java/edu/uga/cs/statecapitalsquiz/StartNewQuizFragment.java
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/StartNewQuizFragment.java
@@ -5,13 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class StartNewQuizFragment extends Fragment {
 
-    public StartNewQuizFragment() {
-        // Required empty public constructor
-    }
+    public StartNewQuizFragment() {}
 
     public static StartNewQuizFragment newInstance() {
         StartNewQuizFragment fragment = new StartNewQuizFragment();
@@ -19,10 +18,14 @@ public class StartNewQuizFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_quiz_layout, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState ) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 }
